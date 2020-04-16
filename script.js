fdfa982418af4c67e26a42155dcb010ec99c47de
Diego Briceño <diegobricep@gmail.com>
2020-04-16 11:38:02 -0500

    Archivo script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var array = [1,2,3,4]; //esto es un objeto
+var persona = {nombre: "Diego",edad:29,activo:true};
+console.log(array);
+console.log(typeof array);
+console.log(persona.edad);
+console.log(typeof persona.edad);
+console.log(persona.nombre);
+console.log(typeof persona.nombre);
+console.log(persona.activo);
+console.log(typeof persona.activo);
