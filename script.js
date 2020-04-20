c90b6c198f78d5e68ca18e09fa76ccf127ceb597
diegobrice <diegobrice@hotmail.com>
2020-04-20 10:37:29 -0500

    tipos de variables

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 var array = [1,2,3,4,5,6]; //esto es un objeto
-var persona = {nombre: "Diego", edad:28, activo:true};
+
+var persona = {
+    nombre: "Diego",
+    edad: 28,
+    activo: true
+};
+
 console.log(array);
 console.log(typeof array);
+console.log(persona);
+console.log(typeof persona);
 console.log(persona.edad);
 console.log(typeof persona.edad);
 console.log(persona.nombre);
 console.log(typeof persona.nombre);
 console.log(persona.activo);
 console.log(typeof persona.activo);
+
+var elementos = ["hola",true,"mundo",2,true, [1,2]];
+console.log(elementos[5]);
+
+elementos.forEach(e => {
+    console.log(typeof e);
+});
+
+
