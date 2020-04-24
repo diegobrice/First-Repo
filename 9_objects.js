db7025b8ff6bf61c2103d3c6a05e1a88052f36da
diegobrice <diegobrice@hotmail.com>
2020-04-23 19:54:47 -0500

    objetos en js

--- 9_objects.js
+++ 9_objects.js
@@ -1,24 +1,24 @@
 // TOPIC: OBJECTS
 
 
-var auto = {
-    marca: "mazda",
-    modelo: "cx-3",
-    distancia: 0,
-    gasolina: 100,
-    año: 2014,
-    avanzar: function(){
-        this.gasolina >= 20 ? (this.distancia += 100, this.gasolina -= 20):(console.log("No hay gasolina"))
-    },
-    llenarTanque: function(num){
-        this.gasolina += num;
-    }
-}
-console.log(auto);
-auto.avanzar();
-console.log(auto);
-auto.llenarTanque(200);
-console.log(auto);
+// var auto = {
+//     marca: "mazda",
+//     modelo: "cx-3",
+//     distancia: 0,
+//     gasolina: 100,
+//     año: 2014,
+//     avanzar: function(){
+//         this.gasolina >= 20 ? (this.distancia += 100, this.gasolina -= 20):(console.log("No hay gasolina"))
+//     },
+//     llenarTanque: function(num){
+//         this.gasolina += num;
+//     }
+// }
+// console.log(auto);
+// auto.avanzar();
+// console.log(auto);
+// auto.llenarTanque(200);
+// console.log(auto);
 
 function automovil(marca,modelo,año) {
     this.marca = marca;
@@ -35,11 +35,43 @@ var modelos = ["Sedan","Hatchback","Pick-up","Van","Camioneta",];
 var años = [2020, 2019, 2018, 2017, 2016];
 var autos = [];
 
-for (var i = 0; i < 5; i++){
+for (var i = 0; i < 20; i++){
     var ale1 = getRandom(0,5), ale2 = getRandom(0,5), ale3 = getRandom(0,5);
     autos.push(new automovil(marcas[ale1],modelos[ale2],años[ale3]));
 }
 console.log(autos);
 console.log(autos.length);
+
+// METODO FILTER PARA FILTRAR UN ARREGLO
+var autosfiltrados = autos.filter(function(auto){
+    return auto.año >= 2018 && (auto.marca == "Toyota" || auto.marca == "Mazda") ;
+});
+console.log(autosfiltrados);
+console.log(autosfiltrados.length);
+
+// METODO MAP
+var marcaAutos = autos.map(function(auto){
+    return auto.marca;
+}); 
+console.log(marcaAutos);
+console.log(marcaAutos.length);
+
+// METODO FIND
+var encuentraAuto = autos.find(function(auto){
+    return auto.marca == "Toyota";
+});
+console.log(encuentraAuto);
+console.log(encuentraAuto.length);
+
+// METODO FOR EACH (recorrido del array)
+autos.forEach(function(auto){
+    console.log(auto.marca,auto.modelo,auto.año);
+});
+
+// METODO SOME (regresa una validacion de true o false)
+var autosModernos = autos.some(function(auto){
+    return auto.año >= 2019;
+})
+console.log(autosModernos);
 
 
