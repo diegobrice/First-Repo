a3184a2ade007f4ea27a630446441eff3331fb8c
diegobrice <diegobrice@hotmail.com>
2020-04-29 18:27:09 -0500

    fundamentos de js

---
+++ Fundamentos/clase1.js
@@ -0,0 +1,34 @@
+var nombre = "Diego", pais = 'Peru', edad = 28;
+
+console.log(`Hola ${nombre} de ${pais} con ${edad}`);
+
+function mayus(a){
+    return a.toUpperCase();
+}
+
+function minus(a){
+    return a.toLowerCase();
+}
+
+// var nomusuario = prompt("¿Cual es tu nombre?");
+// console.log(`La ultima letra de tu nombre es ${nomusuario.charAt(nomusuario.length-1)}`);
+
+var precio = 20.1, cantidad = 3, total;
+total = precio * cantidad;                  //no es tan preciso con los decimales 
+console.log(total);                     
+total = precio * 100 * cantidad / 100       //preciso con 2 decimales
+console.log(total);                     
+total = Math.round(precio * 100 * 3) / 100  //preciso con cualquier decimal
+console.log(total);                     
+var totalStr = total.toFixed(2);            //convertir a 2 decimales
+console.log(totalStr);
+var totalNum = parseFloat(totalStr);        //convertir a float (decimal)
+console.log(totalNum);
+var totalNum = parseInt(totalStr);          //convertir a integer (entero)
+console.log(totalNum);
+var totalNum = Number(totalStr);            //convertir a numero, incluye decimales
+console.log(totalNum); 
+
+console.log(Math.ceil(3.45));               //redondea hacia el mayor
+console.log(Math.round(3.45));              //redondea
+console.log(Math.floor(3.45));              //redondea hacia el menor
