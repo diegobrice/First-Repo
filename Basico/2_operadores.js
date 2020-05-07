49d0b8d298ca84a685d93c0872610a99e5c80a7e
diegobrice <diegobrice@hotmail.com>
2020-05-07 11:23:50 -0500

    asincronismo en js

--- Basico/2_operadores.js
+++ Basico/2_operadores.js
@@ -3,20 +3,20 @@
 // INCREMENTO Y DECREMENTO
 
 var numero = 5;
-++numero;           //numero = numero + 1;
-console.log(numero);      // numero = 6
+++numero; //numero = numero + 1;
+console.log(numero); // numero = 6
 
 var numero = 5;
---numero;           //numero = numero - 1;
-console.log(numero);      // numero = 4
+--numero; //numero = numero - 1;
+console.log(numero); // numero = 4
 
 var numero = 5;
-numero++;           //numero = numero + 1;
-console.log(numero);      // numero = 6
+numero++; //numero = numero + 1;
+console.log(numero); // numero = 6
 
 var numero = 5;
-numero--;           //numero = numero - 1;
-console.log(numero);      // numero = 4
+numero--; //numero = numero - 1;
+console.log(numero); // numero = 4
 
 // Si el operador ++ se indica como prefijo del identificador de la variable, su valor se incrementa antes de realizar cualquier otra operación. Si el operador ++ se indica como sufijo del identificador de la variable, su valor se incrementa después de ejecutar la sentencia en la que aparece.
 
@@ -24,15 +24,15 @@ console.log(numero);      // numero = 4
 
 // NEGACION
 var visible = true;
-console.log(!visible);  // Muestra "false" y no "true"
+console.log(!visible); // Muestra "false" y no "true"
 var cantidad = 0;
-vacio = !cantidad;  // vacio = true
+vacio = !cantidad; // vacio = true
 cantidad = 2;
-vacio = !cantidad;  // vacio = false
+vacio = !cantidad; // vacio = false
 var mensaje = "";
-mensajeVacio = !mensaje;  // mensajeVacio = true
+mensajeVacio = !mensaje; // mensajeVacio = true
 mensaje = "Bienvenido";
-mensajeVacio = !mensaje;  // mensajeVacio = false
+mensajeVacio = !mensaje; // mensajeVacio = false
 
 //AND && Y OR ||
 // La operación lógica AND obtiene su resultado combinando dos valores booleanos. El operador se indica mediante el símbolo && y su resultado solamente es true si los dos operandos son true.
@@ -42,19 +42,19 @@ mensajeVacio = !mensaje;  // mensajeVacio = false
 
 var x = 5;
 var y = 3;
-x += y;   // x = x + y = 8      SUMA
-x -= y;   // x = x - y = 2      RESTA
-x *= y;   // x = x * y = 15     MULTIPLICACION
-x /= y;   // x = x / y = 1.66   DIVISION
-x %= y;   // x = x % y = 2      MODULO 
-x **= y;  // x = x ** y = 125   POTENCIA
+x += y; // x = x + y = 8      SUMA
+x -= y; // x = x - y = 2      RESTA
+x *= y; // x = x * y = 15     MULTIPLICACION
+x /= y; // x = x / y = 1.66   DIVISION
+x %= y; // x = x % y = 2      MODULO
+x **= y; // x = x ** y = 125   POTENCIA
 
 // RELACIONALES
 
 var numero1 = 3;
 var numero2 = 5;
-resultado = numero1 > numero2;  // resultado = false
-resultado = numero1 < numero2;  // resultado = true
+resultado = numero1 > numero2; // resultado = false
+resultado = numero1 < numero2; // resultado = true
 numero1 = 5;
 numero2 = 5;
 resultado = numero1 <= numero2; // resultado = true
@@ -63,8 +63,8 @@ resultado = numero1 != numero2; // resultado = false
 var texto1 = "hola";
 var texto2 = "hola";
 var texto3 = "adios";
-resultado = texto1 == texto3;   // resultado = false
-resultado = texto1 != texto2;   // resultado = false
-resultado = texto3 >= texto2;   // resultado = false
+resultado = texto1 == texto3; // resultado = false
+resultado = texto1 != texto2; // resultado = false
+resultado = texto3 >= texto2; // resultado = false
 
 // Cuando se utilizan cadenas de texto, los operadores "mayor que" (>) y "menor que" (<) siguen un razonamiento no intuitivo: se compara letra a letra comenzando desde la izquierda hasta que se encuentre una diferencia entre las dos cadenas de texto. Para determinar si una letra es mayor o menor que otra, las mayúsculas se consideran menores que las minúsculas y las primeras letras del alfabeto son menores que las últimas (a es menor que b, b es menor que c, A es menor que a, etc.)
---
+++ Fundamentos/asincronismo.js
@@ -0,0 +1,51 @@
+class Persona {
+  constructor(nombre, apellido, edad) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+  }
+  saludar(fn) {
+    // var nombre = this.nombre;
+    // var apellido = this.apellido;
+    // var edad = this.edad;
+    var { nombre, apellido, edad } = this;
+    console.log(`Hola me llamo ${nombre} y tengo ${edad}`);
+    if (fn) {
+      responderSaludo(nombre, apellido);
+    }
+  }
+  soymayor() {
+    return this.edad > 17;
+  }
+  crecer() {
+    this.edad++;
+  }
+}
+
+class Empleado extends Persona {
+  constructor(nombre, apellido, edad) {
+    super(nombre, apellido, edad);
+  }
+  saludar(fn) {
+    console.log(
+      `Hola me llamo ${this.nombre}, soy empleado y tengo ${this.edad}`
+    );
+    if (fn) {
+      responderSaludo(this.nombre, this.apellido, true);
+    }
+  }
+}
+
+function responderSaludo(nombre, apellido, esEmpleado) {
+  console.log(`Buen dia ${nombre}`);
+  if (esEmpleado) {
+    console.log(`Bienvenido empleado`);
+  }
+}
+
+var diego = new Persona("Diego", "Briceño", 17);
+var alonso = new Empleado("Alonso", "Calle", 30);
+diego.saludar();
+alonso.saludar();
+diego.saludar(responderSaludo);
+alonso.saludar(responderSaludo);
